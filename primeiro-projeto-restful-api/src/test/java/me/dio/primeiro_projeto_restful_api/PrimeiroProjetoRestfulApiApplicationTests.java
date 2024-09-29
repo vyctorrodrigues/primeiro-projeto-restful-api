ddbfa376b8a9a57621320b700e859bc2e16a9089
= <=>
2024-09-29 15:21:19 -0300

    projeto

---
+++ primeiro-projeto-restful-api/src/test/java/me/dio/primeiro_projeto_restful_api/PrimeiroProjetoRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.primeiro_projeto_restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
