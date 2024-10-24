ed2fcdf1072826a91f1e4c446fa170afdba97e62
= <=>
2024-10-24 19:38:01 -0300

    adicionando implementações

---
+++ primeiro-projeto-restful-api/src/main/java/me/dio/primeiro_projeto_restful_api/service/UserService.java
@@ -0,0 +1,5 @@
+package me.dio.primeiro_projeto_restful_api.service;
+
+public interface UserService {
+    
+}
---
+++ primeiro-projeto-restful-api/src/main/java/me/dio/primeiro_projeto_restful_api/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package me.dio.primeiro_projeto_restful_api.service.impl;
+
+import me.dio.primeiro_projeto_restful_api.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    
+}
